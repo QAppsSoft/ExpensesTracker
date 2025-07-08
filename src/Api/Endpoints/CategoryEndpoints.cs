2812c2ac5972512b24f5f1f384a53c69c9c061cb
alex6dj <alexeiagueroalba@gmail.com>
2025-07-08 15:07:42 -0300

    Added new endpoints for category operations

--- src/Api/Endpoints/CategoryEndpoints.cs
+++ src/Api/Endpoints/CategoryEndpoints.cs
@@ -1,3 +1,5 @@
+using Api.Models;
+
 namespace Api.Endpoints;
 
 public static class CategoryEndpoints
@@ -13,6 +15,36 @@ public static class CategoryEndpoints
             .WithName("GetCategory")
             .WithDisplayName("Get category")
             .WithDescription("Categories used for expense organization.");
+        
+        group.MapGet("/category/{id:int}", GetCategoryById)
+            .WithName("GetCategoryById")
+            .WithDisplayName("Get category by id")
+            .WithDescription("Get a single category.");
+        
+        group.MapDelete("/category/{id:int}", DeleteCategory)
+            .WithName("DeleteCategory")
+            .WithDisplayName("Delete category")
+            .WithDescription("Delete a single category.");
+        
+        group.MapPost("/category", PostCategory)
+            .WithName("PostCategory")
+            .WithDisplayName("Post category")
+            .WithDescription("Post a new category.");
+    }
+
+    private static Task PostCategory(HttpContext context, Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Task DeleteCategory(HttpContext context, int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Task GetCategoryById(HttpContext context, int id)
+    {
+        throw new NotImplementedException();
     }
 
     private static Task GetCategories(HttpContext context)
