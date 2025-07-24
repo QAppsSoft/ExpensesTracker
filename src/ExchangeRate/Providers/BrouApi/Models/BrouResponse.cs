3c17df717c0de7d637ca4c255388162073982786
alex6dj <alexeiagueroalba@gmail.com>
2025-07-24 11:34:37 -0300

    Updated BrouResponse to handle different base codes and added unit tests for BrouResponse.

--- src/ExchangeRate/Providers/BrouApi/Models/BrouResponse.cs
+++ src/ExchangeRate/Providers/BrouApi/Models/BrouResponse.cs
@@ -8,7 +8,20 @@ public class BrouResponse(BrouRates brouRates, DateTime timeLastUpdate, string b
     public DateTime TimeLastUpdate { get; } = timeLastUpdate;
     public string BaseCode { get; init; } = baseCode;
 
-    public IDictionary<string, decimal> Rates => brouRates.GetAllRates()
-        .Select(x => new KeyValuePair<string, decimal>(x.CurrencyName, x.AskValue))
-        .ToDictionary(x => x.Key, x => x.Value, StringComparer.Ordinal);
+    public IDictionary<string, decimal> Rates => GetRates();
+
+    private Dictionary<string, decimal> GetRates()
+    {
+        var rates = brouRates.GetAllRates();
+
+        if (string.Equals(BaseCode, "UYU", StringComparison.Ordinal))
+        {
+            return rates.Select(x => new KeyValuePair<string, decimal>(x.CurrencyName, x.AskValue))
+                .ToDictionary(x => x.Key, x => x.Value, StringComparer.Ordinal);
+        }
+
+        return rates.Where(x => string.Equals(x.CurrencyName, BaseCode, StringComparison.Ordinal))
+            .Select(x => new KeyValuePair<string, decimal>("UYU", x.BidValue))
+            .ToDictionary(x => x.Key, x => x.Value, StringComparer.Ordinal);
+    }
 }
