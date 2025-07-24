da700529535974a44051f6df99a22697e68397ac
alex6dj <alexeiagueroalba@gmail.com>
2025-07-24 11:04:35 -0300

    Remove unneeded JsonIgnore attribute in BrouResponse.cs

--- src/ExchangeRate/Providers/BrouApi/Models/BrouResponse.cs
+++ src/ExchangeRate/Providers/BrouApi/Models/BrouResponse.cs
@@ -8,7 +8,6 @@ public class BrouResponse(BrouRates brouRates, DateTime timeLastUpdate, string b
     public DateTime TimeLastUpdate { get; } = timeLastUpdate;
     public string BaseCode { get; init; } = baseCode;
 
-    [JsonIgnore]
     public IDictionary<string, decimal> Rates => brouRates.GetAllRates()
         .Select(x => new KeyValuePair<string, decimal>(x.CurrencyName, x.AskValue))
         .ToDictionary(x => x.Key, x => x.Value, StringComparer.Ordinal);
